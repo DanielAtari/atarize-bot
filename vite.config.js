cac60a8a5f8354993c20522262c2b62557862e35
Daniel Atari <danielatari95@gmail.com>
2025-07-23 20:17:44 +0200

    Fix root div and import issues

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: 'static',
-  plugins: [react()],
-  build: {
-    outDir: './dist',
-    emptyOutDir: true,
-  },
-});
+    root: 'static',
+    build: {
+      outDir: 'dist',
+      emptyOutDir: true,
+    },
+    plugins: [react()],
+  })
+  
